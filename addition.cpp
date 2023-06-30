86bd75400cd85b5ad88974a21c73e5935a7fdb6d
yash282004 <138092302+yash282004@users.noreply.github.com>
2023-06-30 08:51:51 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    clrscr();
+    cout<<"Enter two numbers";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"\n addition="<<c;
+    getch();
+}
