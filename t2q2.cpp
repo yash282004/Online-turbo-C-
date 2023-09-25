435ffa8ab97b659b98499faabc1f6d7036331306
yash282004 <138092302+yash282004@users.noreply.github.com>
2023-09-25 10:59:06 +0530

    Update t2q2.cpp

--- t2q2.cpp
+++ t2q2.cpp
@@ -1,33 +1,40 @@
-/*        1
-        1 2 1
-      1 2 3 2 1
-    1 2 3 4 3 2 1
-  1 2 3 4 5 4 3 2 1
-
-
- */
-
-
-
-
-
-
-
-
-#include<iostream.h>
-#include<conio.h>
-int main()
-{
-    int i,j,k;
-    clrscr();
-    for(i=1;i<=5;i++)
-    {
-        for(j=1;j<=i;j++)
-        {
-            cout<<j;
-        }
-        cout<<endl;
-    }
-    getch();
-    return 0;
-}
+/*         1 
+         1 2 1 
+       1 2 3 2 1 
+     1 2 3 4 3 2 1 
+   1 2 3 4 5 4 3 2 1 
+  
+  
+  */ 
+  
+  
+  
+  
+  
+  
+  
+  
+ #include<iostream> 
+ using namespace std;
+ int main() 
+ { 
+     int i,j,k,t; 
+     for(i=1;i<=5;i++) 
+     { 
+         for(k=5;k>i;k--)
+         {
+             cout<<"  ";
+         }
+         for(j=1;j<=i;j++) 
+         { 
+             cout<<j<<" "; 
+         } 
+         for(t=i-1;t>0;t--)
+         {
+             cout<<t<<" ";
+         }
+         cout<<endl; 
+     } 
+     
+     return 0; 
+ }
