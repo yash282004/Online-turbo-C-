4dee13c6e3a860cb134bfef065f17d4cb0fec2c4
yash282004 <138092302+yash282004@users.noreply.github.com>
2023-12-07 16:27:46 +0530

    Update concatetwostringsusingpointer.cpp

--- concatetwostringsusingpointer.cpp
+++ concatetwostringsusingpointer.cpp
@@ -1,28 +1,25 @@
-#include<iostream>
-using namespace std;
+#include<iostream.h>
+#include<conio.h>
 int main()
 {
-    char str1[50],str2[50],str3[100];
-    char *p1,*p2,*p3;
+    char str1[50],str2[100];
+    char *p1,*p2;
     cout<<"Enter string 1:"<<endl;
     cin>>str1;
     cout<<"Enter string 2:"<<endl;
     cin>>str2;
     p1=&str1[0];
     p2=&str2[0];
-    p3=&str3[0];
     while(*p1!='\0')
     {
-        *p3=*p1;
         p1++;
-        p3++;
     }
     while(*p2!='\0')
     {
-        *p3=*p2;
-        p3++;
+        *p1=*p2;
         p2++;
+        p1++;
     }
-    cout<<"string after concatination ="<<str3<<endl;
+    cout<<"string after concatination ="<<str1<<endl;
     return 0;
 }
